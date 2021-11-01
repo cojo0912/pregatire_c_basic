17629e4ce374c3480ac2d8f6a1f55302b3ac162e
andrei.cojocaru <andrei@MacBook-Pro.local>
2021-11-01 12:34:41 +0200

    update

--- 3-nov-2021/main.c
+++ 3-nov-2021/main.c
@@ -3,5 +3,27 @@
 
 int main()
 {
-    
+    char comanda;
+
+    while(1)
+    {
+        printf("Command? "); 
+        scanf("%c", &comanda);
+        switch(comanda)
+        {
+            case 'q':
+                printf("Bye");
+                return 0;
+
+            case '\r':
+            case '\n':
+                break;
+
+            default:
+                printf("Unknown command '%c'", comanda);
+                break;
+        }
+    } 
+
+    return 0;
 }
