04509764506a2901da2f848f20046d29bc4fb7bf
andrei.cojocaru <andrei@MacBook-Pro-2.local>
2021-12-09 17:18:46 +0200

    .

--- 11-dec-2021-ex1/main.c
+++ 11-dec-2021-ex1/main.c
@@ -12,9 +12,57 @@ typedef struct _node_t {
   struct _node_t *nextWord;
 } node_t;
 
+node_t *find_word(node_t *concordance, char word[])
+{
+    while(concordance != NULL)
+    {
+        if(strcmp(concordance -> word, word) == 0)
+            return concordance;
+        concordance = concordance -> nextWord;
+    }
+    return NULL;
+}
+
 node_t *addWord(node_t *concordance, char word[])
 {
-    
+    if(find_page(concordance,word)!=NULL)
+    {
+        printf("URL %s is already exists\n", word);
+        return concordance;
+    }
+
+    struct page * new = (struct page *)malloc(sizeof(node_t));
+    new ->visited = -1;
+    new ->next = NULL;
+    for(int i = 0; i<MAXLINKS; i++)
+        new -> links[i] = NULL;
+    new -> url = (char*)malloc(sizeof(char) * (strlen(url)+1));
+    strcpy(new -> url, url);
+
+    if(web == NULL)
+        return new;
+
+    struct page * iter = web;
+    if(strcmp(iter->url,new->url)>0)
+    {
+        new->next = web;
+        return new;
+    }
+
+    // strcmp(a,b)  <==>   if(a>b)
+    while(iter->next!=NULL)
+    {
+        if(strcmp(iter->next->url,new->url)>0)
+        {
+            new->next = iter->next;
+            iter ->next = new;
+            return web;
+        }
+        iter = iter -> next;
+    }
+
+    iter ->next = new;
+    return web;
 }
 
 int main()
