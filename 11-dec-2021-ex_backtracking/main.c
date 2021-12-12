905300dbba673cf00c57a883700425a563fefaa4
Andrei <andrei.cojocaru@radcom.ro>
2021-12-12 21:36:29 +0200

    .

--- 11-dec-2021-ex_backtracking/main.c
+++ 11-dec-2021-ex_backtracking/main.c
@@ -5,11 +5,90 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+void generare(char * cuv_format, char * alfabetul_multime, int litere_de_adaugat)
+{
+  if(litere_de_adaugat==0)
+  {
+    printf("%s\n", cuv_format);
+    return;
+  }
 
+  for(int i = 0; i<strlen(alfabetul_multime); i++)
+  {
+    cuv_format[litere_de_adaugat-1] = alfabetul_multime[i];
+    generare(cuv_format, alfabetul_multime,litere_de_adaugat-1);
+  }
+}
+
+void generare_ordine(char * cuv_format, char * alfabetul_multime, int poz, int max)
+{
+  if(poz == max)
+  {
+    cuv_format[max] = '\0';
+    printf("%s\n", cuv_format);
+    return;
+  }
+
+  for(int i = 0; i<strlen(alfabetul_multime); i++)
+  {
+    cuv_format[poz] = alfabetul_multime[i];
+    generare_ordine(cuv_format, alfabetul_multime,poz+1, max);
+  }
+}
+
+int isVocala(char a)
+{
+  if(strchr("aeiou",a)!=NULL)
+    return 1;
+  else 
+    return 0;
+}
+
+void generare_ordine_2_vocale(char * cuv_format, char * alfabetul_multime, int poz, int max)
+{
+  if(poz == max)
+  {
+    cuv_format[max] = '\0';
+    printf("%s\n", cuv_format);
+    return;
+  }
+
+  for(int i = 0; i<strlen(alfabetul_multime); i++)
+  {
+    if(poz == 0)
+    {
+      cuv_format[poz] = alfabetul_multime[i];
+      generare_ordine_2_vocale(cuv_format, alfabetul_multime,poz+1, max);
+    }else
+    {
+      if(isVocala(alfabetul_multime[i]) == 1 && isVocala(cuv_format[poz-1] ) == 1)
+      {
+        return;
+      }
+      else
+      {
+        cuv_format[poz] = alfabetul_multime[i];
+        generare_ordine_2_vocale(cuv_format, alfabetul_multime,poz+1, max);
+      }
+    }
+  }
+}
 
 int main()
 {
+  char alph[]="abcde";
+  char cuv[10];
+  //generare(cuv, alph,2);
+  //printf("\n");
+  //generare_ordine(cuv, alph,0,2);
 
+  //printf("%d", isVocala('a'));
+
+  generare_ordine_2_vocale(cuv, alph,0,4);
+
+  
+
+  getc(stdin);
   return 0;
 }
 
--- 11-dec-2021-ex_recursivitate/main.c
+++ 11-dec-2021-ex_recursivitate/main.c
@@ -5,11 +5,78 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+int ex1(int n)
+{
+  if(n==0)
+    return 0;
 
+  return n%10 + ex1(n/10);
+}
+
+int ex2(int n)
+{
+  if(n==0)
+    return 0;
+  
+  return 1 + ex2(n/10);
+}
+
+int ex3(int n)
+{
+  if(n==0)
+    return 0;
+  
+  if(n%2==1)
+  {
+    return 1 + ex3(n/10);
+  }
+  else
+  {
+    return 0 + ex3(n/10);
+  }
+}
+
+void ex4(int n)
+{
+  if(n==0)
+    return;
+
+  ex4(n-1);
+  printf("%d ", n);
+  
+}
+
+int ex5(int n)
+{
+  if(n==0)
+    return 0;
+
+  return ex5(n-1) + n;
+}
+
+int aparitii(int * v, int numar ,int poz)
+{
+  if(poz < 0)
+    return 0;
+  
+  if(v[poz]==numar)
+    return 1 + aparitii(v,numar,poz-1);
+  else
+    return 0 + aparitii(v,numar,poz-1);
+}
 
 int main()
 {
+  printf("%d\n", ex1(1234));
+  printf("%d\n", ex2(1230));
+  printf("%d\n", ex3(1230));
+  ex4(10); printf("\n");
+  printf("%d\n", ex5(4));
 
+  int v[] = {1,1,1,1,2,3,4,5,6};
+  printf("%d\n", aparitii(v,1,8));
+
+  getc(stdin);
   return 0;
 }
 
